5cf726454b664cf847304c6039001fa59f1bb406
Henry <hrmstockdale@gmail.com>
2019-04-04 10:46:48 +0100

    fix/ public no longer hard coded into html

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,11 +1,23 @@
 const handler = require('./handler');
 
+const routeArr = [
+    "/assets/favicon.ico",
+    "/css/normalise.css",
+    "/css/style.css",
+    "/dom.js",
+    "/assets/favicon.ico"
+];
+
+
+
+
+
 const router = (request, response) => {
     const url = request.url;
 
     if(url == '/'){
         handler.handlerHome(request, response);
-    } else if(url.includes('public')){
+    } else if(routeArr.includes(url)){
         handler.handlerPublic(request, response, url);
     } else{
         response.writeHead(404, {'Content-Type': 'text/html'});
